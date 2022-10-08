05a99dbf94c337b85c6c1aa730b08f558a09dfdf
Mattheus Medina Ayala <mataus94@outlook.com>
2022-10-08 16:50:17 -0300

    message

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
-import AboutView from '@/views/AboutView.vue'
-=======
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '@/views/HomeView.vue';
 
->>>>>>> b76963165f5e8ea81a7797f20e0b380aadbba6f1
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
